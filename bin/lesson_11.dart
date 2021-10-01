23b5efa0375977138e78f7082544053e8bdf05c8
RAMZAN PRO <ramzanpro10@gmail.com>
2021-10-01 20:15:54 +0600

    учусь дальше

--- bin/lesson_11.dart
+++ bin/lesson_11.dart
@@ -2,4 +2,8 @@ import 'package:lesson_11/lesson_11.dart' as lesson_11;
 
 void main(List<String> arguments) {
   print('Hello world: ${lesson_11.calculate()}!');
+  int myNumber = 10;
+  if (myNumber == 10) {
+    print('yes it is: $myNumber');
+  }
 }
